c3803030eb6a060b052e43a13f87deb47070dfe6
MilaMeddler <162045635+MilaMeddler@users.noreply.github.com>
2025-12-12 18:06:03 +0100

    Create athlets.js

---
+++ js/athlets.js
@@ -0,0 +1,47 @@
+// api/athletes.js
+import { supabase } from "./supabase.js";
+
+export const AthletesAPI = {
+    async getAll() {
+        const { data, error } = await supabase
+            .from("athletes")
+            .select("*")
+            .order("last_name");
+
+        if (error) throw error;
+        return data;
+    },
+
+    async create(ath) {
+        const { data, error } = await supabase
+            .from("athletes")
+            .insert(ath)
+            .select()
+            .single();
+
+        if (error) throw error;
+        return data;
+    },
+
+    async update(id, dataUpdate) {
+        const { data, error } = await supabase
+            .from("athletes")
+            .update(dataUpdate)
+            .eq("id", id)
+            .select()
+            .single();
+
+        if (error) throw error;
+        return data;
+    },
+
+    async delete(id) {
+        const { error } = await supabase
+            .from("athletes")
+            .delete()
+            .eq("id", id);
+
+        if (error) throw error;
+        return true;
+    }
+};
