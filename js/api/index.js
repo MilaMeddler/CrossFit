f93d156dc7b9b04bea6296c7f7eca1767a652bce
MilaMeddler <162045635+MilaMeddler@users.noreply.github.com>
2025-12-12 18:17:51 +0100

    Create index.js

---
+++ js/api/index.js
@@ -0,0 +1,14 @@
+// api/index.js
+import { AthletesAPI } from "./athletes.js";
+import { TournamentsAPI } from "./tournaments.js";
+import { WodsAPI } from "./wods.js";
+import { ScoresAPI } from "./scores.js";
+import { JudgesAPI } from "./judges.js";
+
+export const api = {
+    athletes: AthletesAPI,
+    tournaments: TournamentsAPI,
+    wods: WodsAPI,
+    scores: ScoresAPI,
+    judges: JudgesAPI
+};
